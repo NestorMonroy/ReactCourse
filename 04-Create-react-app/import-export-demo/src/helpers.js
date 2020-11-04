4f5d617793369f6fea69ba8032fb51311cfc7ce0
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-04 13:17:46 -0600

    example to export component

---
+++ 04-Create-react-app/import-export-demo/src/helpers.js
@@ -0,0 +1,15 @@
+function helpful() {
+	console.log('here!');
+}
+
+function sort() {
+	console.log('ALL SORTED!');
+}
+
+function sing() {
+	console.log('LA LA LA');
+}
+
+
+export default helpful;
+export { sort, sing };
---
+++ 04-Create-react-app/import-export-demo/src/index.js
@@ -0,0 +1,6 @@
+import helpful, {sing, sort} from './helpers';
+
+helpful()
+
+sing()
+sort()

fd585d518fa721da635969fde76d3603ba953cea
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-07 22:39:04 -0600

    adding example shopping list

---
+++ 13-forms-in-react/shopping-list-demo/src/App.js
@@ -0,0 +1,12 @@
+import ShopingList from './ShoppingList'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ShopingList />
+    </div>
+  );
+}
+
+export default App;

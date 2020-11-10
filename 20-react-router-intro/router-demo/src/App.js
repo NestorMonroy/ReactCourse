4f07809783aa2798558cb75991a8535434aaaa47
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-10 17:47:59 -0600

    adding differences betwenn render and component

--- 20-react-router-intro/router-demo/src/App.js
+++ 20-react-router-intro/router-demo/src/App.js
@@ -12,21 +12,35 @@ class App extends Component {
 		return (
 			<div className="App">
 				<nav className="App-nav">
-					<NavLink exact activeClassName="active-link" to="/">
+					<NavLink activeClassName="active-link" to="/">
 						About
 					</NavLink>
-					<NavLink exact activeClassName="active-link" to="/dog">
+					<NavLink activeClassName="active-link" to="/contact">
+						Contact
+					</NavLink>
+					<NavLink activeClassName="active-link" to="/dog">
 						Dog
 					</NavLink>
-					<NavLink exact activeClassName="active-link" to="/contact">
-						Contact
+
+					{/* THESE LAST TWO LINKS ARE FROM THE LAST VIDEO IN THE SECTION! */}
+					{/* ============================================================ */}
+					<NavLink activeClassName="active-link" to="/dog/r">
+						Dog(r)
+					</NavLink>
+					<NavLink activeClassName="active-link" to="/dog/c">
+						Dog(c)
 					</NavLink>
 				</nav>
 				<Switch>
 					<Route exact path="/" component={About} />
-					<Route exact path="/dog" component={Dog} />
 					<Route exact path="/dog/hater" component={Hater} />
 					<Route exact path="/contact" component={Contact} />
+					{/* THESE TWO ROUTES ARE FROM THE LAST VIDEO IN THE SECTION! */}
+					{/* ======================================================== */}
+					{/* Component will instantiate a new Dog every time */}
+					<Route exact path="/dog/c" component={() => <Dog name="Muffins" />} />
+					{/* Render will re-use the existing dog component */}
+					<Route exact path="/dog/r" render={() => <Dog name="Biscuits" />} />
 				</Switch>
 			</div>
 		);
